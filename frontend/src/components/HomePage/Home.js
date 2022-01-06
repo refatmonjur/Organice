2f2e322510788f4468c9e050a5830f0d035a135e
JustinSiu00 <justinsiu1@gmail.com>
2022-01-05 21:01:39 -0500

    finished rough draft of navbar and linked/added paths

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../NavbarPage/Navbar'
 
 function Home() {
     return (
         <div>
-            
+           <div> <Navbar /> </div>
         </div>
+
+       
+
+
     )
 }
 
