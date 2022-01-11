f361d589256beecb02dddc1e16b0a33748232ec9
mdrislam30 <mislam033@citymail.cuny.edu>
2022-01-11 15:19:13 -0500

    new user todo page still in progress

--- frontend/src/components/TodoPage/NewUserTodo.js
+++ frontend/src/components/TodoPage/NewUserTodo.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import Navbar from '../NavbarPage/Navbar'
-
+import React from "react";
+import Navbar from "../NavbarPage/Navbar";
+import "./NewUserTodo.css";
 function NewUserTodo() {
-    return (
-        <div className='newUserTodo'>
-        <div>
-          <Navbar />
-        </div>
+  return (
+    <div className="newUserTodo">
+      <div>
+        <Navbar />
+      </div>
 
-    
-        <div className='newUserTodo__middle'>
-            this is my middle
-        </div>
+      <div className="newUserTodo__middle">
+        this is my middle
+        {/* TEXT */}
+        {/* button */}
+      </div>
 
-        <div className='newUserTodo__buttom'>
-            {/* last section */}
-        </div>
-            
-        </div>
-    )
+      <div className="newUserTodo__buttom">
+        {/* last section */}
+        {/* IMAGE */}
+      </div>
+    </div>
+  );
 }
 
-export default NewUserTodo
-
+export default NewUserTodo;
