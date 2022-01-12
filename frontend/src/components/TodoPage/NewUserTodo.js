eb8ba27cfaa331f6e5b86058a5ff8df1f2beff2a
Refat Monjur <rmonjur000@citymail.cuny.edu>
2022-01-12 01:26:03 -0500

    newtodo

--- frontend/src/components/TodoPage/NewUserTodo.js
+++ frontend/src/components/TodoPage/NewUserTodo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../NavbarPage/Navbar";
 import "./NewUserTodo.css";
+import image from "./task.png";
+
+
 function NewUserTodo() {
   return (
     <div className="newUserTodo">
@@ -14,7 +17,8 @@ function NewUserTodo() {
         {/* button */}
       </div>
 
-      <div className="newUserTodo__buttom">
+      <div className="newUserTodo__bottom">
+      <img src = {image} height = "450" width= "600" />
         {/* last section */}
         {/* IMAGE */}
       </div>
