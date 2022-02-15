40bd5b9d272dcd78a2528936bf17f6edd76fd48b
Md Islam <moe@Mds-MacBook-Pro.local>
2022-02-15 12:44:50 -0500

    show deck Title along with deleting a deck

--- frontend/src/components/FlashcardsPage/DeckConditionalRendering/ShowDecksinPage.js
+++ frontend/src/components/FlashcardsPage/DeckConditionalRendering/ShowDecksinPage.js
@@ -1,54 +1,62 @@
 import React from "react";
 import { Button } from "@mui/material";
 import "../Flashcard.css";
-
-function ShowDecksinPage() {
+import { useState, useEffect } from "react";
+import { useUserAuth } from "../../Context/UserAuthContext";
+import { db } from "../../../firebase.js";
+import { color } from "@mui/system";
+import { decomposeColor } from "@mui/lab/node_modules/@mui/system";
+export default function ShowDecksinPage({ deck, deleting, }) {
 
     // code HERE to render Decks!
 
-    var decks = [];
+    // var decks = [];
 
-    //      -> pulls from user's database to show decks, 
-    //      with buttons to link to the viewing page of the cards 
-    //      -> to let users test themselves as per Organice's goal
-    decks.push("Deck 1"); //sample data
-    decks.push("Deck 2"); //sample data
-    const deckList = decks.map((i) =>
-        <div 
-            className="newUser-text center"
-        >
-            {i}
-            <div>
-                <Button
-                    style={{
+    // //      -> pulls from user's database to show decks, 
+    // //      with buttons to link to the viewing page of the cards 
+    // //      -> to let users test themselves as per Organice's goal
+    // decks.push("Deck 1"); //sample data
+    // decks.push("Deck 2"); //sample data
+    // const deckList = decks.map((i) =>
+    //     <div
+    //         className="newUser-text center"
+    //     >
+    //         {i}
+    //         <div>
+    //             <Button
+    //                 style={{
 
-                    }}
-                >
-                    Edit
-                </Button>
-                <Button
-                    style={{
+    //                 }}
+    //             >
+    //                 Edit
+    //             </Button>
+    //             <Button
+    //                 style={{
 
-                    }}
-                >
-                    Study
-                </Button>
-                <Button
-                    style={{
+    //                 }}
+    //             >
+    //                 Study
+    //             </Button>
+    //             <Button
+    //                 style={{
 
-                    }}
-                >
-                    Delete
-                </Button>
-            </div>
-        </div>
-    );
-
+    //                 }}
+    //             >
+    //                 Delete
+    //             </Button>
+    //         </div>
+    //     </div>
+    // );
+console.log(deck.deckTitle)
 
     return (
-        <div>
-            {deckList}
-        </div>
+<div className="newUser-text center">
+      <div className="testing">{deck.deckTitle}</div>
+      <div>
+        <Button style={{}}>Edit</Button>
+        <Button style={{}}>Study</Button>
+        <Button onClick={() => deleting(deck.id)}>Delete</Button>
+      </div>
+    </div>
     );
 }
-export default ShowDecksinPage;
--- frontend/src/components/FlashcardsPage/Flashcard.js
+++ frontend/src/components/FlashcardsPage/Flashcard.js
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import NewHomeNavbar from "../NavbarPage/NewHomeNavbar";
 import { Button } from "@mui/material";
 import "./Flashcard.css";
 import NoDeckPage from "./DeckConditionalRendering/NoDeckPage.js";
 import ShowDecksinPage from "./DeckConditionalRendering/ShowDecksinPage.js";
 import { Link } from "react-router-dom";
-
+import { useUserAuth } from "../Context/UserAuthContext";
+import { db } from "../../firebase.js";
+import { collection, onSnapshot, doc, deleteDoc } from "firebase/firestore";
 function Flashcard() {
   // state = {
   //   hasDeck: true
   // }
   const [hasDeck, sethasDeck] = useState(true);
-
+  const { user } = useUserAuth();
+  const [decks, setDecks] = useState([]);
+  // const [Loading, setLoading] = useState(false);
   // Testing variable - delete after implementing backend code for this page
   // var hasDecksinData = this.state.hasDeck; //takes in a Boolean
   // if false, it will say user has no deck
   // if true, it will say user HAS a deck
   // either way User can still create a new deck always to add to their collection
-
+  useEffect(() => {
+    // setLoading(true);
+    const DecksCollectionRef = collection(db, "user", user.uid, "flashcard");
+    // const todoQuery = query(DecksCollectionRef, orderBy("timeStamp", "desc"));
+    const unsub1 = onSnapshot(DecksCollectionRef, (queryS) => {
+      const decksArray = [];
+      queryS.forEach((doc) => {
+        decksArray.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(decksArray);
+      setDecks(decksArray);
+      console.log(decks);
+    });
+    return () => unsub1();
+  }, []);
+  const deleting = async (id) => {
+    const docRef = doc(db, "user", user.uid, "flashcard", id);
+    await deleteDoc(docRef);
+  };
   return (
     <div>
       <div>
@@ -38,7 +61,13 @@ function Flashcard() {
         </Button>
 
         {/* SHOW THIS DIV IF User has no decks in their data */}
-        <div>{hasDeck ? <ShowDecksinPage /> : <NoDeckPage />}</div>
+        <div>
+        {decks.map((deck) => (
+            <ShowDecksinPage key={deck.id} deck={deck} deleting={deleting} />
+          ))}
+          {/* <ShowDecksinPage key={deck.id} deck={deck} deleting={deleting}/> */}
+          {/* {hasDeck ? <ShowDecksinPage /> : <NoDeckPage />} */}
+          </div> 
       </div>
     </div>
   );
