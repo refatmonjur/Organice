8682f6253a37ca92ad3d5b7632f0b352d859de3c
Md Islam <moe@Mds-MacBook-Pro.local>
2022-03-01 12:57:30 -0500

    edited code for uploading files, still needs work, same issues

--- frontend/src/components/FlashcardsPage/AddImageCard.js
+++ frontend/src/components/FlashcardsPage/AddImageCard.js
@@ -65,84 +65,80 @@ const AddImageCard = () => {
   const [word, setWord] = useState("");
   const [definition, setDefinition] = useState("");
   const [purpose, setPurpose] = useState("");
-  const [url, setUrl] = useState(null);
+  const [uurl, setUrl] = useState(null);
   const [file, setFile] = useState(null);
   const types = ["application/pdf", "image/png", "image/jpeg"];
   const [error, setError] = useState(null);
   const [inputFields, setInputField] = useState([
     { word: "", definition: "", purpose: "", url: "" },
-    { word: "", definition: "", purpose: "", url: "" },
   ]);
-  // const [progress, setProgress] = useState(0);
-  console.log(url);
-  async function changeUpload(index, e) {
-    console.log("changed");
-    let selected = e.target.files[0];
-    console.log(selected);
+  const [progress, setProgress] = useState(0);
+  // console.log(url);
+  // async function changeUpload(index, e) {
+  //   console.log("changed");
+  //   let selected = e.target.files[0];
+  //   console.log(selected);
 
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError("");
-      // let lol = useStorage(file, inputFields);
-      // console.log(lol);
-      // const handleChangeInput1 = (index, e) => {
-      //   const values = [...inputFields];
-      //   console.log(url);
-      //   values[index][e.target.name] = userData.getUrl();
-      //   setInputField(values);
-      // };
-      // handleChangeInput1(index, e);
-      console.log(selected);
-    } else {
-      setFile(null);
-      setError("Please select an image file or pdf file (png,jepg,pdf)");
-    }
-    const handleChangeInput1 = (index, e) => {
-      console.log("i am here");
-      const values = [...inputFields];
-      console.log(userData.getUrl());
-      console.log(url);
-      values[index][e.target.name] = userData.getUrl();
-      setInputField(values);
-    };
-    handleChangeInput1(index, e);
-    console.log(inputFields);
-    console.log(selected);
-  }
-
-  // const uploadFiles = (file) => {
-  //   if (!file) return;
-  //   const storageRef = ref(storage, `/files/${file.name}`);
-  //   const uploadTask = uploadBytesResumable(storageRef, file);
-
-  //   uploadTask.on("state_changed", (snapshot) => {
-  //     const prog = Math.round((snapshot.byteTransferred / snapshot.totalBytes) * 100);
-  //     setProgress(prog);
-  //   }, (err) => console.log(err),
-  //     () => {
-  //       getDownloadURL(uploadTask.snapshot.ref)
-  //         .then((downloadURL) => {
-  //           setFile(downloadURL);
-  //     });
+  //   if (selected && types.includes(selected.type)) {
+  //     setFile(selected);
+  //     setError("");
+  //     // let lol = useStorage(file, inputFields);
+  //     // console.log(lol);
+  //     // const handleChangeInput1 = (index, e) => {
+  //     //   const values = [...inputFields];
+  //     //   console.log(url);
+  //     //   values[index][e.target.name] = userData.getUrl();
+  //     //   setInputField(values);
+  //     // };
+  //     // handleChangeInput1(index, e);
+  //     console.log(selected);
+  //   } else {
+  //     setFile(null);
+  //     setError("Please select an image file or pdf file (png,jepg,pdf)");
   //   }
-  //   );
+  //   const handleChangeInput1 = (index, e) => {
+  //     console.log("i am here");
+  //     const values = [...inputFields];
+  //     console.log(userData.getUrl());
+  //     values[index][e.target.name] = userData.getUrl();
+  //     setInputField(values);
+  //   };
+  //   handleChangeInput1(index, e);
+  //   console.log(inputFields);
+  //   console.log(selected);
   // }
+
+  const uploadFiles = (file) => {
+    if (!file) return;
+    const storageRef = ref(storage, `/files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const prog = Math.round((snapshot.byteTransferred / snapshot.totalBytes) * 100);
+      setProgress(prog);
+    }, (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            setUrl(downloadURL);
+            
+      });
+      console.log(uurl)
+    }
+    );
+  }
 
   const createDeck = async (e) => {
     e.preventDefault();
-    // const FlashCardRefs = collection(db, "user", user.uid, "flashcard");
-    // const url= await storageRef.getDownloadURL();
-    // setURL(url);
-    // changeUpload(file);
-    const FlashCardRefs = doc(db, "user", user.uid, "flashcard", deckName);
-    var data = {
-      deckTitle: deckName,
-    };
+    // const FlashCardRefs = doc(db, "user", user.uid, "flashcard", deckName);
+    // var data = {
+    //   deckTitle: deckName,
+    // };
 
-    if (deckName !== "") {
-      await setDoc(FlashCardRefs, data);
-      console.log("check firebase");
-    }
+    // if (deckName !== "") {
+    //   await setDoc(FlashCardRefs, data);
+    //   console.log("check firebase");
+    // }
     // var data1 = {
     //   word: word,
     //   definition: definition,
@@ -158,28 +154,29 @@ const AddImageCard = () => {
       deckName,
       "deck"
     );
-    for (let i = 0; i < inputFields.length; i++) {
-      await addDoc(decksrefs, inputFields[i]);
-      console.log("check firebase");
+    if (deckName !== "") {
+      for (let i = 0; i < inputFields.length; i++) {
+        await addDoc(decksrefs, inputFields[i]);
+        console.log("check firebase");
+      }
     }
+    
   };
   const handleChangeInput = (index, event) => {
     const values = [...inputFields];
     values[index][event.target.name] = event.target.value;
     setInputField(values);
   };
-  const handleChangeInput2 = (index, event) => {
-    const values = [...inputFields];
-    values[index][event.target.name] = url;
-    setInputField(values);
-    console.log(inputFields);
+  console.log(uurl)
+  const handleChangeInput2 = (index, e) => {
+    const values1 = [...inputFields];
+    console.log("i am here")
+    values1[index][e.target[6].name] = uurl;
+    console.log(values1)
+    setInputField(values1);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // const file = e.target[0].files[0];
-    // uploadFiles(file);
-  };
+
 
   const handleAddFields = (index) => {
     setInputField([
@@ -193,7 +190,14 @@ const AddImageCard = () => {
     values.splice(index, 1);
     setInputField(values);
   };
-  console.log(url);
+  const handleSubmit = (index, e) => {
+    e.preventDefault();
+    let file = e.target[6].files[0];
+    uploadFiles(file);
+    handleChangeInput2(index, e)
+  };
+  console.log(uurl);
+  console.log(inputFields);
 
   return (
     <div>
@@ -217,10 +221,9 @@ const AddImageCard = () => {
           />
         </div>
       </div>
-
-      {/* Question Answer Add Cards Div */}
-      <form onSubmit={handleSubmit}>
-        {inputFields.map((inputField, index) => (
+      <div>
+    {inputFields.map((inputField, index)=>(
+        <form onSubmit={(e)=> handleSubmit(index, e)}>
           <div key={index}>
             <div className="addnewdeck-header center-text">
               <div id="flex-containerQA">
@@ -251,7 +254,7 @@ const AddImageCard = () => {
                   // onChange={(e) => setPurpose(e.target.value)}
                   onChange={(event) => handleChangeInput(index, event)}
                 />
-                <div
+                {/* <div
                   className="output"
                   style={{
                     fontWeight: "normal",
@@ -268,7 +271,7 @@ const AddImageCard = () => {
                       setUrl={setUrl}
                     />
                   )}
-                </div>
+                </div> */}
 
                 <div
                   className="whiteBg center"
@@ -284,30 +287,28 @@ const AddImageCard = () => {
                       type="file"
                       className="file-upload-button"
                       name="url"
-                      onChange={(e) => changeUpload(index, e)}
                       style={{
                         color: "blue",
                         marginBottom: 30,
                       }}
                     />
-                    {/* <h1> {inputField.url}</h1> */}
+                    <h1> the file is uploading {progress}</h1>
+                      <img src={inputField.url}>
+                      </img>
 
-                    {/* <button> 
-                      Upload
-                    </button> */}
-
-                    <Button
+                    <button
                       // onClick={uploadFiles(file)}
-                      onClick={(event) => handleChangeInput2(index, event)}
-                      style={{
-                        backgroundImage:
-                          "linear-gradient(89.97deg, #cea9f5 1.84%, #F49867 102.67%)",
-                        color: "blue",
-                        fontWeight: 500,
-                      }}
+                      type="submit"
+                      // style={{
+                      //   backgroundImage:
+                      //     "linear-gradient(89.97deg, #cea9f5 1.84%, #F49867 102.67%)",
+                      //   color: "blue",
+                      //   fontWeight: 500,
+                      // }}
+                      
                     >
                       Upload
-                    </Button>
+                    </button>
                   </div>
                   <div>
                     <h4
@@ -340,8 +341,9 @@ const AddImageCard = () => {
               <AddIcon />
             </IconButton>
           </div>
-        ))}
       </form>
+      ))}
+      </div>
       <div id="flex-containerbtns">
         {/* <Button className="add-card-btn" style={{ marginTop: 20 }}>
           Add Card
@@ -350,7 +352,7 @@ const AddImageCard = () => {
         <Button
           className="finish-deck-btn"
           style={{ marginTop: 20 }}
-          OnClick={createDeck}
+          OnClick={(e)=>createDeck(e)}
         >
           Finish & Save
         </Button>
--- frontend/src/components/FlashcardsPage/DeckConditionalRendering/ProgressBar.js
+++ frontend/src/components/FlashcardsPage/DeckConditionalRendering/ProgressBar.js
@@ -1,31 +1,31 @@
-import React, { useEffect } from "react";
-import useStorage from "./useStorage";
-import { userData } from "../../Context/UserData";
+// import React, { useEffect } from "react";
+// import useStorage from "./useStorage";
+// import { userData } from "../../Context/UserData";
 
-function ProgressBar({ file, setFile, setUrl }) {
-  const { progress, url } = useStorage(file);
+// function ProgressBar({ file, setFile, setUrl }) {
+//   const { progress, url } = useStorage(file);
 
-  console.log(progress);
-  console.log(url);
-  useEffect(
-    () => {
-      if (url) {
-        setFile(null);
-        setUrl(userData.getUrl());
-        // userData.setUrl(url);
-      }
-    },
-    [url],
-    setFile
-  );
-  // function lol() {
-  //   return url;
-  // }
-  return (
-    <div className="progress-bar" style={{ width: progress + "%" }}>
-      <h2>{progress}</h2>
-    </div>
-  );
-}
+//   console.log(progress);
+//   console.log(url);
+//   useEffect(
+//     () => {
+//       if (url) {
+//         setFile(null);
+//         setUrl(userData.getUrl());
+//         // userData.setUrl(url);
+//       }
+//     },
+//     [url],
+//     setFile
+//   );
+//   // function lol() {
+//   //   return url;
+//   // }
+//   return (
+//     <div className="progress-bar" style={{ width: progress + "%" }}>
+//       <h2>{progress}</h2>
+//     </div>
+//   );
+// }
 
-export default ProgressBar;
+// export default ProgressBar;
