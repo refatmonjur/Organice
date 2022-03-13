dde6c6faa361aee5c9fde3c59a0c586738310bcf
mdrislam30 <mislam033@citymail.cuny.edu>
2022-03-12 22:10:43 -0500

    study flashcard is completed

--- frontend/src/components/FlashcardsPage/StudyEachCard.js
+++ frontend/src/components/FlashcardsPage/StudyEachCard.js
@@ -1,30 +1,91 @@
 import React from "react";
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 import { useState, useEffect, useRef } from "react";
-function StudyEachCard({deckName}) {
+import { db } from "../../firebase";
+import { useUserAuth } from "../Context/UserAuthContext";
+import {
+  collection,
+  getDoc,
+  doc,
+  onSnapshot,
+  addDoc,
+  query,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+} from "firebase/firestore";
+import { handleBreakpoints } from "@mui/system";
+
+function StudyEachCard({ deckName }) {
   // this is the component for studying flashcards and changing to next card
   const ref = useRef();
+  const { user } = useUserAuth();
+  const [flashCards, setFlashCards] = useState([]);
+  const [currentCard, setCurrentCard] = useState(0);
+  useEffect(() => {
+    const DeckCollectionRef = collection(
+      db,
+      "user",
+      user.uid,
+      "flashcard",
+      deckName,
+      "deck"
+    );
+    const unsub = onSnapshot(DeckCollectionRef, (queryS) => {
+      const FlashCardsArray = [];
+      queryS.forEach((doc) => {
+        FlashCardsArray.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(FlashCardsArray);
+      setFlashCards(FlashCardsArray);
+    });
+
+    return () => unsub();
+  }, []);
+
   return (
     <div>
-      <div className=" flashcard-deck-title gradient__text">
-              {deckName}
-            </div>
+      <div className=" flashcard-deck-title gradient__text">{deckName}</div>
       <div>
-      <Flippy
-                flipOnClick={true} // default false 
-                flipDirection="vertical"
-                ref={ref} 
-                style={{ minWidth: '200px', height: '200px' }} /// these are optional style, it is not necessary
-              >
-                <FrontSide style={{backgroundColor: '#41669d', textAlign: "center"}}> 
-                  <p>Prompt</p>
-                </FrontSide>
+        <button
+          onClick={() => {
+            if (currentCard > 0) {
+              setCurrentCard(currentCard - 1);
+            }
+          }}
+        >
+          prev
+        </button>
+      </div>
+      <div>
+        <Flippy
+          flipOnClick={true} // default false
+          flipDirection="vertical"
+          ref={ref}
+          style={{ minWidth: "200px", height: "200px" }} /// these are optional style, it is not necessary
+        >
+          <FrontSide
+            style={{ backgroundColor: "#41669d", textAlign: "center" }}
+          >
+            {flashCards.length > 0 && <p>{flashCards[currentCard].question}</p>}
+          </FrontSide>
 
-                <BackSide style={{ backgroundColor: '#FFFFFF', textAlign: "center"}}>
-                  Answer
-                </BackSide>
-
-              </Flippy>
+          <BackSide style={{ backgroundColor: "#FFFFFF", textAlign: "center" }}>
+            {flashCards.length > 0 && <p>{flashCards[currentCard].answer}</p>}
+            {/* <p>{flashCards[currentCard].answer}</p> */}
+          </BackSide>
+        </Flippy>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (currentCard + 1 < flashCards.length) {
+              setCurrentCard(currentCard + 1);
+            }
+          }}
+        >
+          next
+        </button>
       </div>
     </div>
   );
